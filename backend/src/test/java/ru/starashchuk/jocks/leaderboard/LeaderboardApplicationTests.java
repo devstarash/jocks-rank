c7ef606b84fb0219880b91ffe2a54440f0d66e0a
Nikita Starashchuk <nik.starashchuk@bk.ru>
2026-06-24 15:04:44 +0800

    chore: Инициализация Spring Boot API приложения

---
+++ backend/src/test/java/ru/starashchuk/jocks/leaderboard/LeaderboardApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.starashchuk.jocks.leaderboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeaderboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
